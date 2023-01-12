eaf1c4c672f39aff1e8260b5d776cbe2cf7edc2e
gerlyntan07 <115803839+gerlyntan07@users.noreply.github.com>
2023-01-12 10:11:51 +0800

    Add files via upload

---
+++ Homepage/Homepage.Master.vb
@@ -0,0 +1,11 @@
+﻿Public Class Homepage
+    Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Private Sub btnReserve_Click(sender As Object, e As EventArgs) Handles btnReserve.Click
+        Response.Redirect("Reservation.aspx")
+    End Sub
+End Class
--- Homepage/Homepage.aspx
+++ Homepage/Homepage.aspx
@@ -1,1 +1,3 @@
-
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Homepage.Master" CodeBehind="Homepage.aspx.vb" Inherits="F5_HotelReservationSystem.Homepage1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
